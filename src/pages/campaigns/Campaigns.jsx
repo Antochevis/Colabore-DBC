b37e07c06acb88ed52bfb31a836647fc60db1e48
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-13 20:43:49 -0300

    puxando userDatas na page campanhas e adicionando loading.

--- src/pages/campaigns/Campaigns.jsx
+++ src/pages/campaigns/Campaigns.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { apiColabore } from '../../services/api'
+import Loading from '../../components/Loading/Loading'
 
 function Campaigns() {
-  return (
-    <div>Campaigns</div>
-  )
+  const [user, setUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  const setup = async () => {
+    try {
+      const { data } = await apiColabore.get('usuario/listar')
+      setUser(data[0])
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    setup()
+  },[])
+  
+  if(loading) {
+    return (<Loading />)
+  } 
+    return (
+      <h1>Seja bem vindo {user.nome}</h1>
+    )
 }
 
 export default Campaigns
