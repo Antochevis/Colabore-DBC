b593889d632ec36d0eb4113ffe64d01879c8dbd5
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-16 12:56:34 -0300

    Validação senha.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -7,24 +7,7 @@ import { BackgroundRegister, RegisterButtonFormStyle, RegisterContainer, Registe
 import { useNavigate } from 'react-router-dom';
 import { Signup } from "../login/Login.Styled";
 
-const SignupSchema = yup.object().shape({
-  nome: yup.string()
-    .min(2, 'Mínimo de 2 caractéres')
-    .max(50, 'Máximo de 50 caractéres')
-    .required('Campo obrigatório!'),
-  email: yup.string()
-    .min(2, 'Mínimo de 2 caractéres')
-    .max(50, 'Máximo de 50 caractéres')
-    .required('Campo obrigatório!'),
-  senha: yup.string()
-    .min(2, 'Mínimo de 2 caractéres')
-    .max(50, 'Máximo de 50 caractéres')
-    .required('Campo obrigatório!'),
-  confirmarSenha: yup.string()
-    .min(2, 'Mínimo de 2 caractéres')
-    .max(50, 'Máximo de 50 caractéres')
-    .required('Campo obrigatório!')
-})
+
 
 function Register() {
   //const navigate = useNavigate();
@@ -41,6 +24,30 @@ function Register() {
   const { handleSignUp } = useContext(AuthContext);
   const navigate = useNavigate();
   const [image, setImage] = useState()
+  const [valueSenha, setSenha] = useState()
+  const [valueConfirmaSenha, setConfirmaSenha] = useState()
+
+  console.log(valueSenha, valueConfirmaSenha )
+
+  const SignupSchema = yup.object().shape({
+    nome: yup.string()
+      .min(2, 'Mínimo de 2 caractéres')
+      .max(50, 'Máximo de 50 caractéres')
+      .required('Campo obrigatório!'),
+    email: yup.string()
+      .min(2, 'Mínimo de 2 caractéres')
+      .max(50, 'Máximo de 50 caractéres')
+      .required('Campo obrigatório!'),
+    senha: yup.string()
+      .min(8, 'Mínimo de 8 caractéres')
+      .max(16, 'Máximo de 16 caractéres')
+      .required('Campo obrigatório!'),
+    confirmarSenha: yup.string()
+      .min(8, 'Mínimo de 8 caractéres')
+      .max(16, 'Máximo de 16 caractéres')
+      .oneOf([yup.ref('senha'), null], 'As senhas precisam ser iguais.')
+      .required('Campo obrigatório!')
+  })
 
   return (
     <BackgroundRegister>
@@ -89,12 +96,12 @@ function Register() {
               </div>
               <div>
                 <label htmlFor="senha">Senha*</label>
-                <Field type='password' name='senha' placeholder='Digite sua senha'/>
+                <Field type='password' name='senha' placeholder='Digite sua senha' onBlur={(e) => {setSenha(e.target.value)}}/>
                 {errors.senha && touched.senha ? (<Errors>{errors.senha}</Errors>) : null}
               </div>
               <div>
                 <label htmlFor="confirmarSenha">Confirmar senha*</label>
-                <Field type='password' name='confirmarSenha' placeholder='Confirme sua senha'/>
+                <Field type='password' name='confirmarSenha' placeholder='Confirme sua senha' onBlur={(e) => {setConfirmaSenha(e.target.value)}}/>
                 {errors.confirmarSenha && touched.confirmarSenha ? (<Errors>{errors.confirmarSenha}</Errors>) : null}
               </div>
               <RegisterButtonFormStyle disabled={errors.nome || errors.email || errors.senha || errors.confirmarSenha} type='submit'>Cadastrar</RegisterButtonFormStyle>
